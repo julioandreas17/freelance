fe956e1064481b1a787d363c259984807f93e37f
julio andreas <julioandreas17@gmail.com>
2025-12-07 03:40:09 +0700

    SEE setup

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,4 @@
+var action = {};
 AOS.init();
 
 $('body').find('.about-us').on('click', 'div[role="button"]', function (){
@@ -18,3 +19,67 @@ $('body').find('.about-us').on('click', 'div[role="button"]', function (){
   element.find('.' + target).removeClass('d-none').fadeIn();
   AOS.init();
 });
+
+function open_menu_top (){
+  let element = $('.menu-top');
+  element.removeClass('position-relative')
+  .addClass('position-fixed')
+  .addClass('start-0')
+  .addClass('end-0')
+  .addClass('top-0')
+  .addClass('w-100')
+  .addClass('shadow')
+  .css('z-index', 7)
+  .css('margin-top', '-'+ element[0].scrollHeight +'px');
+  setTimeout(function (){
+    element.css('transition', '0.5s')
+    .css('margin-top', '0px');
+  }, 1);
+
+  if (action['close_menu_top'] != undefined){
+    delete action['close_menu_top'];
+  }
+}
+
+function close_menu_top (){
+  let element = $('.menu-top');
+  element.addClass('position-relative')
+  .removeClass('position-fixed')
+  .removeClass('start-0')
+  .removeClass('end-0')
+  .removeClass('top-0')
+  .removeClass('w-100')
+  .removeClass('shadow')
+  .removeAttr('style');
+
+  if (action['open_menu_top'] != undefined){
+    delete action['open_menu_top'];
+  }
+}
+
+function stand_menu_top (){
+  let element = $('.stand-menu-top');
+  let reference = $('.menu-top');
+  let body = $('html, body');
+  element.css('height', reference[0].scrollHeight + 'px');
+
+  if (body.scrollTop() > element[0].scrollHeight + 20){
+    if (action['open_menu_top'] == undefined){
+      action['open_menu_top'] = 'run';
+      open_menu_top();
+    }
+  }else{
+    if (action['close_menu_top'] == undefined){
+      action['close_menu_top'] = 'run';
+      close_menu_top();
+    }
+  }
+}
+
+function render (){
+  stand_menu_top();
+  requestAnimationFrame(function (){
+    render();
+  });
+}
+render();
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,74 +1,76 @@
-<div class="menu-top position-relative bg-white w-100 pb-3">
-  <div class="container">
-    <div class="row d-flex align-items-center">
-      <div class="col-auto logo-area">
-        <a href="{!! url('./') !!}" class="text-decoration-none d-block">
+<div class="stand-menu-top position-relative w-100 bg-white">
+  <div class="menu-top position-relative bg-white w-100 pb-3">
+    <div class="container">
+      <div class="row d-flex align-items-center">
+        <div class="col-auto logo-area">
+          <a href="{!! url('./') !!}" class="text-decoration-none d-block">
+            <div class="row d-flex align-items-center">
+              <div class="col-auto mt-3">
+                <img src="{!! url(env('ICON').''.$version) !!}" style="border-radius: 10px;width: 60px;">
+              </div>
+              <div class="col-auto mt-3 px-0">
+                <div class="text-primary fw-bold">
+                  PT. SELARAS ENERGI <span class="text-secondary">ELEKTRIKAL</span>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col mt-3">&nbsp;</div>
+        <div class="col-auto menu-area">
           <div class="row d-flex align-items-center">
             <div class="col-auto mt-3">
-              <img src="{!! url(env('ICON').''.$version) !!}" style="border-radius: 10px;width: 60px;">
+              <a href="{!! url('./') !!}" class="text-decoration-none d-block">
+                <div class="position-relative">
+                  <div class="position-absolute start-0 end-0 bottom-0 rounded-pill bg-primary" style="height: 2px;"></div>
+                  <div class="text-primary p-2 text-truncate" style="border-radius: 5px;">
+                    Home
+                  </div>
+                </div>
+              </a>
             </div>
-            <div class="col-auto mt-3 px-0">
-              <div class="text-primary fw-bold">
-                PT. SELARAS ENERGI <span class="text-secondary">ELEKTRIKAL</span>
-              </div>
+            <div class="col-auto mt-3">
+              <a href="{!! url('/about_us') !!}" class="text-decoration-none d-block">
+                <div class="position-relative">
+                  <div class="text-secondary p-2 text-truncate" style="border-radius: 5px;">
+                    About Us
+                  </div>
+                </div>
+              </a>
             </div>
-          </div>
-        </a>
-      </div>
-      <div class="col mt-3">&nbsp;</div>
-      <div class="col-auto menu-area">
-        <div class="row d-flex align-items-center">
-          <div class="col-auto mt-3">
-            <a href="{!! url('./') !!}" class="text-decoration-none d-block">
-              <div class="position-relative">
-                <div class="position-absolute start-0 end-0 bottom-0 rounded-pill bg-primary" style="height: 2px;"></div>
-                <div class="text-primary p-2 text-truncate" style="border-radius: 5px;">
-                  Home
+            <div class="col-auto mt-3">
+              <a href="{!! url('/our_products') !!}" class="text-decoration-none d-block">
+                <div class="position-relative">
+                  <div class="text-secondary p-2 text-truncate" style="border-radius: 5px;">
+                    Our Products
+                  </div>
                 </div>
-              </div>
-            </a>
-          </div>
-          <div class="col-auto mt-3">
-            <a href="{!! url('/about_us') !!}" class="text-decoration-none d-block">
-              <div class="position-relative">
-                <div class="text-secondary p-2 text-truncate" style="border-radius: 5px;">
-                  About Us
+              </a>
+            </div>
+            <div class="col-auto mt-3">
+              <a href="{!! url('/contact_us') !!}" class="text-decoration-none d-block">
+                <div class="position-relative">
+                  <div class="text-secondary p-2 text-truncate" style="border-radius: 5px;">
+                    Contact Us
+                  </div>
                 </div>
-              </div>
-            </a>
-          </div>
-          <div class="col-auto mt-3">
-            <a href="{!! url('/our_products') !!}" class="text-decoration-none d-block">
-              <div class="position-relative">
-                <div class="text-secondary p-2 text-truncate" style="border-radius: 5px;">
-                  Our Products
-                </div>
-              </div>
-            </a>
-          </div>
-          <div class="col-auto mt-3">
-            <a href="{!! url('/contact_us') !!}" class="text-decoration-none d-block">
-              <div class="position-relative">
-                <div class="text-secondary p-2 text-truncate" style="border-radius: 5px;">
-                  Contact Us
-                </div>
-              </div>
-            </a>
+              </a>
+            </div>
           </div>
         </div>
-      </div>
-      <div class="col-auto theme-area">
-        <div class="row d-flex align-items-center">
-          <div class="col-auto mt-3">
-            <i class="bi bi-sun text-secondary"></i>
-          </div>
-          <div class="col-auto mt-3 px-0">
-            <div class="form-check form-switch">
-              <input class="form-check-input" type="checkbox" id="theme" name="theme">
+        <div class="col-auto theme-area">
+          <div class="row d-flex align-items-center">
+            <div class="col-auto mt-3">
+              <i class="bi bi-sun text-secondary"></i>
             </div>
-          </div>
-          <div class="col-auto mt-3">
-            <i class="bi bi-moon-stars text-secondary"></i>
+            <div class="col-auto mt-3 px-0">
+              <div class="form-check form-switch">
+                <input class="form-check-input" type="checkbox" id="theme" name="theme">
+              </div>
+            </div>
+            <div class="col-auto mt-3">
+              <i class="bi bi-moon-stars text-secondary"></i>
+            </div>
           </div>
         </div>
       </div>
